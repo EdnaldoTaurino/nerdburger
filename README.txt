dc0e452b63529e6a45197d11a03c2f7ee32e6549
EdnaldoTaurino <ednaldo.taurino5@gmail.com>
2023-05-28 22:08:20 -0300

    Adicionado pasta assets, login e menu,

--- README.txt
+++ README.txt
@@ -6,3 +6,12 @@ adicionei a pasta CSS
 fiz o css do menu e do fundo da pagina
 fiz o menu do site com a parte de login e cadastro
 adicionei o fave.icon (icone do site)
+
+
+28/05/2023
+o que fiz hoje:
+adicionei a pasta assets com bootstrap
+fiz o html base do login/cadastro e do menu
+fiz o css do menu
+fiz o css do login
+adicionei mais uma imagem a pasta img
---
+++ menu/script.js
@@ -0,0 +1,29 @@
+var total = 0;
+var totalElement = document.getElementById('total');
+var sacola = document.getElementById('itens-sacola');
+
+function adicionarItem(item, preco) {
+  var novoItem = document.createElement('li');
+  novoItem.innerText = item;
+  novoItem.className = 'sacola-item';
+  novoItem.onclick = function() {
+    sacola.removeChild(novoItem);
+    total -= preco;
+    atualizarTotal();
+  };
+  sacola.appendChild(novoItem);
+  total += preco;
+  atualizarTotal();
+}
+
+function atualizarTotal() {
+  totalElement.innerText = 'Total: R$' + total.toFixed(2);
+}
+
+function fazerPedido() {
+  if (total > 0) {
+    alert('Pedido realizado! Total: R$' + total.toFixed(2));
+  } else {
+    alert('A sacola está vazia. Adicione itens antes de fazer o pedido.');
+  }
+}
