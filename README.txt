efdb82d8573a0b2be5747c88a94d4629ac670e7f
EdnaldoTaurino <ednaldo.taurino5@gmail.com>
2023-06-14 15:34:11 -0300

    Remove para att main

--- README.txt
+++
@@ -1,5 +0,0 @@
-Ednaldo - Cardápio.
-Francisco - Pagina inicial.
-Roseanne - Prototiagem, Fale conosco.
-Michelly - prototipagem.
-Victor - Menu lateral, Login, Cadastro e Meu perfil.
--- js/script.js
+++
@@ -1,8 +0,0 @@
-//Cria uma função que mostra e esconde a barra lateral
-
-// busca os elementos da class "sidebar-button", atribui a variável button e adiciona um listener para monitorar, quando houver um click ele ativa a função hide
-var button = document.querySelector('.sidebar-button').addEventListener("click", hide); 
-var box = document.querySelector('.sidebar-box')
-function hide(){
-    box.classList.toggle('hidden') // remove a class "hidden" do "sidebar-box" assim, retirando as propriedades que foram declaradas no CSS que escondiam a barra lateral.
-}
